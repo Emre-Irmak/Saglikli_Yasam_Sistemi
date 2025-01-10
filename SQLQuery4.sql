560a62e25d661b91b257aff032bc9269f79d3282
Emre-Irmak <163827876+Emre-Irmak@users.noreply.github.com>
2025-01-11 00:02:29 +0300

    Add files via upload

---
+++ SQLQuery4.sql
@@ -0,0 +1,28 @@
+--Bu prosedür, bir kullanýcýnýn egzersiz kaydýný ekler ve kullanýcýyla ilgili ilerleme bilgilerini güncelleyerek, egzersiz sonrasý geliþim takibi yapar.
+--Prosedür tanýmladýk ve parametreleri ekledik
+CREATE PROCEDURE AddUserExercise
+    @KullaniciID INT,
+    @HareketID INT,
+    @EgzersizSuresi INT,
+    @Tarih DATE
+AS
+BEGIN
+    -- Kullanýcý egzersiz kaydý ekleme
+    INSERT INTO UserExercises (KullaniciID, HareketID, EgzersizSuresi, Tarih)
+    VALUES (@KullaniciID, @HareketID, @EgzersizSuresi, @Tarih);
+
+    -- Kullanýcýnýn ilerleme bilgilerini tutmak için deðiþken tanýmlama
+    DECLARE @Kilo DECIMAL(5,2);
+    DECLARE @VucutYagOrani DECIMAL(5,2);
+    DECLARE @KasKutlesi DECIMAL(5,2);
+
+    -- Kullanýcýnýn son ilerleme bilgilerini alýr
+    SELECT TOP 1 @Kilo = Kilo, @VucutYagOrani = VucutYagOrani, @KasKutlesi = KasKutlesi  --top1 sadece en son kaydý getirir deðiþkenlere deðer atar
+    FROM ProgressTracking
+    WHERE KullaniciID = @KullaniciID
+    ORDER BY Tarih DESC;
+
+    -- Kullanýcýnýn kaydettiðimiz yeni ilerleme kaydýný ProgressTracking tablosuna ekler
+    INSERT INTO ProgressTracking (KullaniciID, Kilo, VucutYagOrani, KasKutlesi, Tarih)
+    VALUES (@KullaniciID, @Kilo, @VucutYagOrani, @KasKutlesi, @Tarih);
+END;
